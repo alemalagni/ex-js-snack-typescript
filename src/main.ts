3328d71d7f5a6e54cd9afe681646581fc46658b8
alemalagni <alemalagni@gmail.com>
2025-09-26 10:54:49 +0200

    refactor Dato function to handle string, number, and boolean types

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
-let dato: string | number | boolean = 'Hola Mundo';
-
-console.log(typeof dato === 'string' ? dato.toUpperCase() : '');
+function Dato(dato: string | number | boolean) {
+  console.log(typeof dato === 'string' ? dato.toUpperCase() : '');
+  console.log(typeof dato === 'number' ? dato * 2 : '')
+  console.log(typeof dato === 'boolean' ? (dato ? 'Sì' : 'No') : '');
+}
